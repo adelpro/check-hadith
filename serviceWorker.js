d58ad60dbdb1c9fa0a28de4cf4e3ce6b442893d4
adelpro <adelpro@gmail.com>
2022-02-21 16:46:01 +0100

    v2.0

---
+++ serviceWorker.js
@@ -0,0 +1,76 @@
+const staticCache = "Checkhadith-cache-v6";
+const dynamicCache = "Checkhadith-dynamic-v6";
+const assets = [
+  "/",
+  "./index.html",
+  "./fallback.html",
+  "./404.html",
+  "./style.css",
+  "./favicon.ico",
+  "./images/16x16.png",
+  "./images/32x32.png",
+  "./images/48x48.png",
+  "./images/128x128.png",
+  "./images/64x64.png",
+  "./images/90x90.png",
+  "./images/256x256.png",
+  "./images/512x512.png",
+  "./images/monochrome.png",
+  "./images/192x192.png"
+];
+// cache size limit function
+const limitCacheSize = (name, size) => {
+  caches.open(name).then((cache) => {
+    cache.keys().then((keys) => {
+      if (keys.length > size) {
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
+
+self.addEventListener("install", (installEvent) => {
+  installEvent.waitUntil(
+    caches.open(staticCache).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+// activate event
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== staticCache && key !== dynamicCache)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+// fetch event
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    caches
+      .match(evt.request)
+      .then((cacheRes) => {
+        return (
+          cacheRes ||
+          fetch(evt.request).then((fetchRes) => {
+            return caches.open(dynamicCache).then((cache) => {
+              cache.put(evt.request.url, fetchRes.clone());
+              // check cached items size
+              limitCacheSize(dynamicCache, 30);
+              return fetchRes;
+            });
+          })
+        );
+      })
+      .catch(() => {
+        if (evt.request.url.indexOf(".html") > -1) {
+          return caches.match("/fallback.html");
+        }
+      })
+  );
+});
